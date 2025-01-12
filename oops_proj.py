e92db3b671ce2079813f4a334ed460119171c118
Harsh Kumar <harshks2320@gmail.com>
2025-01-12 20:42:25 +0530

    Covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -10,19 +10,21 @@ class chatbook:
     def menu(self):
         user_input = input("""Welcome to Chatbook !! How would you like to proceed
                                 1. Press 1 to signup
-                                2. Press 1 to signin
+                                2. Press 2 to signin
                                 3. Press 3 to write a post
                                 4. Press 4 to message a friend
-                                5. Press any other key to exit""")
+                                5. Press any other key to exit
+                                
+                                ->""")
 
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else:
             exit()
 
@@ -53,5 +55,29 @@ class chatbook:
         print("\n")
         self.menu()
 
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here -> ")
+            print(f"Following content has been posted -> {txt}")
 
-obj = chatbook()
+        else:
+            print("You need to signin first to post something")
+
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg? -> ")
+            print(f"Your message has been sent to {frnd}")
+
+        else:
+            print("You need to signin to post something...")
+
+        print("\n")
+        self.menu()
+
+
+# user1 = chatbook()
+
--- rough.py
+++ rough.py
@@ -1,7 +1,11 @@
-lst = [1, 2, 3]
-my_str = "mlosp playlist"
-my_int = 155
+# lst = [1, 2, 3]
+# my_str = "mlosp playlist"
+# my_int = 155
 
-# print(type(my_int))
-lst.clear
-print(lst)
+# # print(type(my_int))
+# lst.clear
+# print(lst)
+
+from  oops_proj import chatbook
+
+user1 = chatbook()
